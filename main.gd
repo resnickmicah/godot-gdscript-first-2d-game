d6c1307a8cc51be9b48f19eb41b5310234218e3a
Micah Resnick <resnickmicah@gmail.com>
2025-07-12 19:39:27 -0400

    Add HUD to Main scene
    Finished: https://docs.godotengine.org/en/stable/getting_started/first_2d_game/06.heads_up_display.html
    Up next: https://docs.godotengine.org/en/stable/getting_started/first_2d_game/07.finishing-up.html

--- main.gd
+++ main.gd
@@ -13,11 +13,16 @@ func _process(delta: float) -> void:
 func game_over():
     $ScoreTimer.stop()
     $MobTimer.stop()
+    $HUD.show_game_over()
+    get_tree().call_group("mobs", "queue_free")
+
 
 func new_game():
     score = 0
     $Player.start($StartPosition.position)
     $StartTimer.start()
+    $HUD.update_score(score)
+    $HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout():
     # Create a new instance of the Mob scene.
@@ -46,7 +51,12 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
     score += 1
+    $HUD.update_score(score)
 
 func _on_start_timer_timeout():
     $MobTimer.start()
     $ScoreTimer.start()
+
+
+func _on_hud_start_game() -> void:
+    pass # Replace with function body.
